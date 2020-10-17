67bcba69ffb1a6175bb4ee1c314596630cb44f62
shakeebbb <36937615+shakeebbb@users.noreply.github.com>
2020-10-17 17:45:57 -0600

    First commit

---
+++ include/scan_plan.h
@@ -0,0 +1,19 @@
+#ifndef SCANPLAN_H
+#define SCANPLAN_H
+
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/PoseArray.h"
+
+class scan_plan_class
+{
+
+private:
+
+public:
+
+
+};
+
+#endif
